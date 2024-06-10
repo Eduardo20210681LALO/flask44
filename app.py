48502ea998051ccf216c016bb927319c26df78bd
Eduardo20210681LALO <20210681@uthh.edu.mx>
2024-06-10 02:38:11 -0600

    Practica Serpientes tarea1

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 # Cargar el modelo entrenado
-model = joblib.load('model.pkl')
+model = joblib.load('modelo1.pkl')
 app.logger.debug('Modelo cargado correctamente.')
 
 @app.route('/')
@@ -19,23 +19,27 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Obtener los datos enviados en el request
-        abdomen = float(request.form['abdomen'])
-        antena = float(request.form['antena'])
+        # Obtener los datos enviados en el request y convertirlos a flotantes
+        genus = float(request.form['genus'])
+        class_id = float(request.form['class_id'])
+        snake_sub_family = float(request.form['snake_sub_family'])
+        poisonous = float(request.form['poisonous'])
         
         # Crear un DataFrame con los datos
-        data_df = pd.DataFrame([[abdomen, antena]], columns=['abdomen', 'antena'])
+        data_df = pd.DataFrame([[genus, class_id, snake_sub_family, poisonous]], columns=['genus', 'class_id', 'snake_sub_family', 'poisonous'])
         app.logger.debug(f'DataFrame creado: {data_df}')
         
         # Realizar predicciones
         prediction = model.predict(data_df)
-        app.logger.debug(f'Predicción: {prediction[0]}')
+        predicted_class = str(prediction[0])  # Convertir la predicción a cadena
+        
+        app.logger.debug(f'Predicción: {predicted_class}')
         
         # Devolver las predicciones como respuesta JSON
-        return jsonify({'categoria': prediction[0]})
+        return jsonify({'categoria': predicted_class})
     except Exception as e:
         app.logger.error(f'Error en la predicción: {str(e)}')
-        return jsonify({'error': str(e)}), 400
+        return jsonify({'error': 'Error en la solicitud. Detalles en el registro del servidor.'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
